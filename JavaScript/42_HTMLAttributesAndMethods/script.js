ffad7e7e16d85954e88f505f0694fb01e4a009fc
SaiKumarTheppa <saikumartheppa06@gmail.com>
2025-03-28 14:53:13 +0530

    HTML Attributes and their methods

---
+++ JavaScript/42_HTMLAttributesAndMethods/script.js
@@ -0,0 +1,23 @@
+let a = document.getElementById('first');
+console.log(a.getAttribute('class'));
+console.log(first.hasAttribute('hidden'));
+console.log(first.hasAttribute('style'));
+
+// Set Attribute
+
+// first.setAttribute("hidden"  ,"true");
+
+// first.setAttribute("class" , "second third");
+
+// first.removeAttribute("class");
+
+
+console.log(first.attributes);
+
+
+
+// Data -* attributes
+console.log(first.dataset);
+console.log(first.dataset.game);
+console.log(first.dataset.player);
+
