90db7598ac70992a90434dfdb3eaadb2453edb33
SaiKumarTheppa <saikumartheppa06@gmail.com>
2025-03-29 18:46:35 +0530

    Intoduction browser events and Hangling Browser Events

---
+++ JavaScript/47_IntroductionToBrowserEvents/scripts.js
@@ -0,0 +1,6 @@
+let button = document.getElementsByTagName('button')[0];
+button.onclick = () =>{
+    button.style.background = 'red';
+    button.style.color = 'white';
+    button.innerHTML = 'Button Clicked';
+}
---
+++ JavaScript/48_HandlingBrowserEvents/script.js
@@ -0,0 +1,18 @@
+let a = function(e){
+    alert("Hello World 1");
+    console.log(e);
+    console.log(e.target);
+    console.log(e.type , e.clientX , e.clientY);
+
+}
+let b = function(e){
+    alert("Hello World 2");
+}
+let input = Number.parseInt(prompt("Enter a number"));
+
+let btn = document.getElementsByTagName('button')[0];
+btn.addEventListener('click' , a);
+// btn.addEventListener('click' , b);
+// if(input == 2){
+//     btn.removeEventListener('click' , b);
+// }
