c9fa2fe1f975a2a1b49d5a8aa89fc963a6f6486c
SaiKumarTheppa <saikumartheppa06@gmail.com>
2025-03-31 22:52:27 +0530

    Synchronous and Asynchronous in JS

--- JavaScript/52_IntroductionToCallBacks/script.js
+++ JavaScript/52_IntroductionToCallBacks/script.js
@@ -1,11 +1,11 @@
 // Synchronous Programming -> Actions that initiate and finish one-by-one
 
-// let a = prompt("What is your Gender");
-// let b = prompt("What is your age");
-// let c = prompt("What is your favourite color");
-// console.log(a + b + c );
+let a = prompt("What is your Gender");
+let b = prompt("What is your age");
+let c = prompt("What is your favourite color");
+console.log(a + b + c );
 
-// Asynchronous Programming -> Actions that initiate now and finish Later
+// Asynchronous Programming -> Actions that initiate now and finish Later.
 console.log("start");
 setTimeout(function(){
     console.log("Asynchronous Example");
