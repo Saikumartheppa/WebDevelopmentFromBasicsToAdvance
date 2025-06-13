1e632a3e865fc2f38c7ca6db88c5471bf35071c3
SaiKumarTheppa06 <sai.kumar@entropik.io>
2025-06-13 22:46:27 +0530

    Constructors 02

---
+++ JavaScript/80_Constructors02/script.js
@@ -0,0 +1,44 @@
+class Employee{
+    constructor(name , age){
+        console.log("Employee Constructor is Executed");
+        this.name = name;
+        this.age = age;
+    }
+    login(){
+        console.log(`${this.name}  Logged in Successfully`);
+    }
+    logout(){
+        console.log(`${this.name}  Logged out Successfully`);
+    }
+}
+
+// class newEmployee extends Employee{
+//     constructor(name , age , role){
+//         super(name , age);
+//         this.role = role;
+//     }  
+//     login(){
+//         console.log(`${this.name} is Logged in 10 minutes Earlier`);
+//     }
+//     designation(){
+//         console.log(`${this.name}  designation is  ${this.role}`);
+//     }
+// }
+
+class newEmployee extends Employee{
+    constructor(name , age  , role){
+        super();
+        this.role = role;
+    }  
+    login(){
+        console.log(`${this.name} is Logged in 10 minutes Earlier`); // Name will be undefined
+    }
+    designation(){
+        console.log(`${this.name}  designation is  ${this.role}`); // Name will be undefined
+    }
+}
+// let oldEmployee = new Employee("Kumar" , 25);
+let newEmp = new newEmployee("Sai" , 24 , "FrontEnd Dev");
+
+newEmp.login();
+newEmp.designation();
