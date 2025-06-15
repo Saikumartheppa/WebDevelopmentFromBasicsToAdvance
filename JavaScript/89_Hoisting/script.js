14489a60967ae294fd3057685286a85bf8b76de0
SaiKumarTheppa06 <sai.kumar@entropik.io>
2025-06-15 12:47:08 +0530

    Hoisting in JS

---
+++ JavaScript/89_Hoisting/script.js
@@ -0,0 +1,13 @@
+
+// console.log(a);
+
+//  var a;
+// let a;
+// const a = 9;
+greet();
+// function greet(){
+//     console.log("Hellow");
+// }
+const greet = ()=>{
+    console.log("Hellow");      // function expressions are not hoisted. function expressions are those which uses () => expressions
+}
